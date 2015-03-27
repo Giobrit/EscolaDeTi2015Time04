af84d2bcc492dad9b7bc2468ed1218bd00bcb677
renato.nakamura <renato.kenji.nakamura@gmail.com>
2015-03-26 22:10:12 -0300

    Adicionando projeto ao repositorio

---
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/Application.java
@@ -0,0 +1,17 @@
+package br.unicesumar.escoladeti2015time04;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author escoladeti2015time04
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
