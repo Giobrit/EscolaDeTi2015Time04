0918154f33ce6db1b90b0b373b5cf60f8cb0963a
giovanni.dias <gio_br@live.com>
2015-04-16 18:55:00 -0300

    issue#5052 arrumando post do frontend

--- escoladeti2015time04/src/main/webapp/js/cadastroUsuario.js
+++ escoladeti2015time04/src/main/webapp/js/cadastroUsuario.js
@@ -1,39 +1,39 @@
 //module = angular.module("app", []);
-AppModule.controller("cadastroUsuarioController", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
+AppModule.controller("cadastroUsuarioController", ["$scope", "$http", "$routeParams", "$log", function ($scope, $http, $routeParams, $log) {
 
         //$scope.isNovo = true;
-        
-         $scope.submitForm = function () {
+        $scope.usuario = {};
+        $scope.submitForm = function () {
 
-                // verifica se o formulÃ¡rio Ã© vÃ¡lido
-                if ($scope.userForm.$valid) {
-                    $scope.salvar = function salvar() {
-                        $http.post("/usuario", $scope.login)
-                            .success(function () {
-                                alert("Sucesso!");
-                              $scope.atualizar();
-                                novo();
-                            }).error(deuErro);
+            // verifica se o formulÃ¡rio Ã© vÃ¡lido
+            if ($scope.userForm.$valid) {
+                $scope.salvar();
+            }
+        };
 
-                            }
-                        }
-                    };
+        $scope.salvar = function () {
+            $scope.usuario = {
+                "login": $scope.user.username,
+                "senha": $scope.user.password,
+                "email": $scope.user.email,
+                "status": "ATIVO",
+                "nome": $scope.user.name
+            };
 
-        $scope.salvar = function salvar() {
-            $http.post("/usuario", $scope.username)
-                   .success(function () {
-                       alert("Sucesso!");
-                     $scope.atualizar();
-                       novo();
-                   }).error(deuErro);
+            $log.log(JSON.stringify($scope.usuario));
+            $http.post("/usuario", $scope.usuario)
+                    .success(function () {
+                        alert("Sucesso!");
+//                        $scope.atualizar();
+                        $scope.novo();
+                    }).error(deuErro);
 
-          
+        }
 
-       }
-
-       function deuErro() {
-           alert("Lerigo!!!");
-      };
+        function deuErro(data) {
+            alert(JSON.stringify(data) + "Lerigo!!!");
+        }
+        ;
 //      function  OnFocusOutForm(event){
 //	senha = document.cadastroUsuario.senha.value;
 //	confirmarSenha = document.cadastroUsuario.confirmarSenha.value;
@@ -45,7 +45,9 @@ AppModule.controller("cadastroUsuarioController", ["$scope", "$http", "$routePar
 //	}
 //};
 
-
+           $scope.novo = function() {
+               $scope.user = {};
+           };
 
 
     }]);
