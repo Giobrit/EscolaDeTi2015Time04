8e675f861facb0b4e01570b8875d1eb3dcc33812
roney.campos <roney.1995@hotmail.com>
2015-05-03 21:24:32 -0300

    issue#5314 - Implementando bootstrap da listagem de usuários.

--- escoladeti2015time04/src/main/webapp/js/listUsuario.js
+++ escoladeti2015time04/src/main/webapp/js/listUsuario.js
@@ -1,23 +1,27 @@
-AppModule.controller("UsuarioList",["$scope","$routeParams", function ($scope, $routeParams){
-         
-            $scope.login = {};
-            $scope.name = {};
-            $scope.email = {};
-            
-            $scope.user = [
-                { login : "Joao123", name : "Joao da Silva", email : "joao@hotmail.com" },
-                { login : "hhhh", name : "Hhhh da hhh", email : "hhhh@hhh.com" },
-                { login : "Maria12", name : "Maria da Silva", email : "mariazinha@uol.com" }
-            ];
-            
-           $scope.init = function(){
-               $scope.limpar();
-            };
+AppModule.controller("UsuarioList", ["$scope", "$routeParams", function ($scope, $routeParams) {
+
+        $scope.login = {};
+        $scope.name = {};
+        $scope.email = {};
+
+        $scope.usuarios = [
+            {login: "Joao123", nome: "Joao da Silva", email: "joao@hotmail.com", ativo: true},
+            {login: "hhhh", nome: "Hhhh da hhh", email: "hhhh@hhh.com", ativo: false},
+            {login: "Maria12", nome: "Maria da Silva", email: "mariazinha@uol.com", ativo: true}
+        ];
+
+        $scope.alterarStatus = function (usuario) {
+            usuario.ativo = !usuario.ativo;
+        };
+
+        $scope.init = function () {
+            $scope.limpar();
+        };
 //            
 
 //            
-           $scope.remove = function(id) {
-            alert("Remover o Id:" + id);           
-            };
+        $scope.remove = function (id) {
+            alert("Remover o Id:" + id);
+        };
 
-}]);
+    }]);
