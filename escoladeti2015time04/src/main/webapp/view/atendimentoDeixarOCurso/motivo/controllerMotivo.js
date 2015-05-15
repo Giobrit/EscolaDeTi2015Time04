2c0c2c3f28ed4eb1f6e53301a56959d0e1e50800
liz.okuzono <lizokuzono@gmail.com>
2015-05-15 17:30:39 -0300

    #5384 Implementacao do front end com controllers do atendimentoDeixarOCurso/Motivo

---
+++ escoladeti2015time04/src/main/webapp/view/atendimentoDeixarOCurso/motivo/controllerMotivo.js
@@ -0,0 +1,113 @@
+AppModule.controller("controllerFormMotivoAtendimentoDeixarOCurso", controllerFormMotivoAtendimentoDeixarOCurso);
+
+AppModule.controller("controllerListMotivoAtendimentoDeixarOCurso", controllerListMotivoAtendimentoDeixarOCurso );
+
+function controllerFormMotivoAtendimentoDeixarOCurso($scope, $http, $routeParams, $location) {
+
+    $scope.init = function () {
+        limparTela();
+
+        idMotivoEditado = $routeParams.id;
+        if (idMotivooEditado) {
+            $scope.editando = true;
+            $scope.editar(idMotivoEditado);
+        }
+    };
+
+
+    $scope.salvar = function () {
+        if ($scope.editando) {
+            $http.put("/atendimento/deixarOCurso/motivo", $scope.motivo).success(onSuccess).error(onError);
+        } else {
+            $http.post("/atendimento/deixarOCurso/motivo", $scope.motivo).success(onSuccess).error(onError);
+        }
+
+        function onSuccess() {
+            $location.path("/AtendimentoDeixarOCurso/Motivo/list");
+            alert("Motivo cadastrado com sucesso!");
+        }
+    };
+
+    $scope.editar = function (id) {
+        $http.get("/atendimento/deixarOCurso/motivo/" + id).success(onSuccess).error(onError);
+
+        function onSuccess(data) {
+            $scope.motivo = data;
+        }
+    };
+
+    function limparTela() {
+        $scope.motivo = {};
+        $scope.editando = false;
+    }
+
+    function onError(data) {
+        alert(JSON.stringify(data));
+    }
+
+}
+
+function controllerListMotivoAtendimentoDeixarOCurso($scope, $http) {
+
+    $scope.paginaAtual = 1;
+    $scope.totalPaginas = 1;
+    $scope.colunaOrdenacao = "descricao";
+    var ordenacaoCrescente = true;
+
+    $scope.init = function () {
+        $scope.listar();
+    };
+    
+    $scope.alterarStatus = function (motivo) {
+        var status = motivo.status === 'ATIVO' ? 'INATIVO' : 'ATIVO';
+        $http.put("/atendimento/deixarOCurso/motivo/" + motivo.id + "/" + status).success(onSuccess).error(onError);
+
+        function onSuccess() {
+            motivo.status = status;
+        }
+    };
+
+    $scope.listar = function () {
+        var requisicaoListagem = new RequisicaoListagem();
+        requisicaoListagem.numeroItens = 8;
+        requisicaoListagem.paginaAtual = $scope.paginaAtual;
+        requisicaoListagem.colunaOrdenacao = $scope.colunaOrdenacao;
+        requisicaoListagem.ordenacaoCrescente = ordenacaoCrescente;
+        requisicaoListagem.valorFiltragem = $scope.pesquisa;
+        
+        $scope.motivos = [{"id":1,"descricao":"ola","status":"ATIVO"},{"id":2,"descricao":"tchau","status":"ATIVO"}, {"id":3,"descricao":"oidenovo","status":"INATIVO"}];
+        return;
+        
+        $http.post("/atendimento/deixarOCurso/motivo/listar", requisicaoListagem).success(onSuccess).error(onError);
+        function onSuccess(data) {
+            $scope.motivos = data.itens;
+            $scope.totalPaginas = data.numeroTotalPaginas;
+        }
+    };
+ 
+     $scope.alterarOrdenacao = function (coluna) {
+        if ($scope.colunaOrdenacao === coluna) {
+            ordenacaoCrescente = !ordenacaoCrescente;
+        }
+        
+        $scope.colunaOrdenacao = coluna;
+        $scope.listar();
+    };
+    
+    $scope.alterarPagina = function (pagina) {
+        if (pagina < 1) {
+            return;
+        }
+
+        if (pagina > $scope.totalPaginas) {
+            return;
+        }
+
+        $scope.paginaAtual = pagina;
+        $scope.listar();
+    };
+   
+    function onError(data) {
+        alert(JSON.stringify(data));
+    }
+}
