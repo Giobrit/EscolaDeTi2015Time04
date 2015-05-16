b8f25300f55a9752bcc30a886977f06e2050068e
giovanni.dias <gio_br@live.com>
2015-05-16 15:01:42 -0300

    #5724 Arrumando o web service para trabalhar numa porta diferente do projeto principal.

--- webServiceLyceum/src/main/java/br/unicesumar/webservicelyceum/aluno/AlunoService.java
+++ webServiceLyceum/src/main/java/br/unicesumar/webservicelyceum/aluno/AlunoService.java
@@ -38,7 +38,57 @@ class AlunoService {
             aluno.matriculado = "Sim";
             aluno.bolsa = "10% sarandi/CREDIN";
             aluno.reprovacao = "0";
-        } 
+        } else if (id == 11002782) {
+            aluno.ra = "11002782";
+            aluno.nome = "Luiz Gustavo Sabaine Fagundes";
+            aluno.centro = "CETA";
+            aluno.curso = "SISIN";
+            aluno.serie = "3ª serie";
+            aluno.turno = "Noturno";
+            aluno.matriculado = "Sim";
+            aluno.bolsa = "NÃO";
+            aluno.reprovacao = "0";
+        } else if (id == 13078102) {
+            aluno.ra = "13078102";
+            aluno.nome = "Willian Zanuto Oliveira";
+            aluno.centro = "CETA";
+            aluno.curso = "SISIN";
+            aluno.serie = "3ª serie";
+            aluno.turno = "Noturno";
+            aluno.matriculado = "Sim";
+            aluno.bolsa = "50% PROMUBE";
+            aluno.reprovacao = "0";
+        } else if (id == 13089252) {
+            aluno.ra = "13089252";
+            aluno.nome = "Filipe Martins Maldinado";
+            aluno.centro = "CETA";
+            aluno.curso = "ADS";
+            aluno.serie = "3ª serie";
+            aluno.turno = "Noturno";
+            aluno.matriculado = "Sim";
+            aluno.bolsa = "100% PROMUBE";
+            aluno.reprovacao = "0";
+        } else if (id == 13003052) {
+            aluno.ra = "13003052";
+            aluno.nome = "Liz Regina Okuzono";
+            aluno.centro = "CETA";
+            aluno.curso = "ADS";
+            aluno.serie = "3ª serie";
+            aluno.turno = "Noturno";
+            aluno.matriculado = "Sim";
+            aluno.bolsa = "100% FIES";
+            aluno.reprovacao = "0";
+        } else if (id == 13097572) {
+            aluno.ra = "13097572";
+            aluno.nome = "Rodrigo Ferreira de Souza";
+            aluno.centro = "CETA";
+            aluno.curso = "ADS";
+            aluno.serie = "3ª serie";
+            aluno.turno = "Noturno";
+            aluno.matriculado = "Sim";
+            aluno.bolsa = "100% FIES";
+            aluno.reprovacao = "0";
+        }
         return aluno;
     }
 }
