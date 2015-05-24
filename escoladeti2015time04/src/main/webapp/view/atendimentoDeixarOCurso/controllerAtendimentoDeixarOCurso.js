3dd567e3d2dadfd3419e3d9e45370e8c986d64fb
renato.nakamura <renato.kenji.nakamura@gmail.com>
2015-05-24 19:44:55 -0300

    #5510 Fazendo acoplamento formAtendimentoDeixarOCurso

--- escoladeti2015time04/src/main/webapp/view/atendimentoDeixarOCurso/controllerAtendimentoDeixarOCurso.js
+++ escoladeti2015time04/src/main/webapp/view/atendimentoDeixarOCurso/controllerAtendimentoDeixarOCurso.js
@@ -4,6 +4,11 @@ function controllerFormAtendimentoDeixarOCurso($scope, $http, $routeParams, $loc
     
     $scope.init = function(){
         limparTela();
+        preencherListDeObjetivo();
+        preencherListDeMotivo();
+        
+        alert(JSON.stringify($scope.objetivos));
+        alert(JSON.stringify($scope.motivos));
         
         idEditando = $routeParams.id;
         
@@ -19,13 +24,14 @@ function controllerFormAtendimentoDeixarOCurso($scope, $http, $routeParams, $loc
     };
     
     $scope.salvar = function () {        
+        
         if ($scope.editando) {
             $http.put("/atendimento/deixarOCurso", $scope.atendimentoDeixarOCurso).success(onSuccess).error(onError);
         } else {
             $http.post("/atendimento/deixarOCurso", $scope.atendimentoDeixarOCurso).success(onSuccess).error(onError);
         }
 
-        function onSuccess() {
+        function onSuccess() {            
             alert(JSON.stringify($scope.atendimentoDeixarOCurso));
             $location.path("/atendimento/deixarOCurso/list");
             alert("Atendimento salvo com sucesso");
@@ -37,6 +43,24 @@ function controllerFormAtendimentoDeixarOCurso($scope, $http, $routeParams, $loc
         
         function onSuccess(data){            
             $scope.atendimentoDeixarOCurso = data;
+        }
+    };
+    
+    $scope.preencherListDeObjetivo = function (){
+        var requisicaoListagem = new RequisicaoListagem();
+        $http.post("atendimento/deixarOCurso/objetivo/listar", requisicaoListagem).success(onSuccess).error(onError);
+        
+        function onSuccess(data) {
+            $scope.objetivos = data.itens;            
+        }
+    };
+    
+    $scope.preencherListDeMotivo = function (){
+        var requisicaoListagem = new RequisicaoListagem();
+        $http.post("/atendimento/deixarOCurso/motivo/listar", requisicaoListagem).success(onSuccess).error(onError);
+        
+        function onSuccess(data) {
+            $scope.motivos = data.itens;       
         }
     };
     
