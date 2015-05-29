6000847541f2e7d4e4a7fd651f2c6f1d1e381f6e
giovanni.dias <gio_br@live.com>
2015-05-29 04:48:50 -0300

    #5872 Alterando a senha do arquivo para 123 que é o usado pela maioria; Alterando as entidades de Atendimento para que a herança funcione; Alterando os Commands de Atendimento para trabalharem com herança; Alterando o Service generico para trabalhar com entidades que possuem herança;

--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/atendimento/deixarOCurso/AtendimentoDeixarOCurso.java
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/atendimento/deixarOCurso/AtendimentoDeixarOCurso.java
@@ -1,6 +1,6 @@
-
 package br.unicesumar.escoladeti2015time04.atendimento.deixarOCurso;
 
+import br.unicesumar.escoladeti2015time04.atendimento.Atendimento;
 import br.unicesumar.escoladeti2015time04.utils.listagem.ColunaListavel;
 import br.unicesumar.escoladeti2015time04.atendimento.deixarOCurso.motivo.*;
 import br.unicesumar.escoladeti2015time04.atendimento.deixarOCurso.objetivo.*;
@@ -8,49 +8,42 @@ import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "atendimentodeixarocurso")
-public class AtendimentoDeixarOCurso implements Serializable {
-  
-    @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
-    private Long id;
-    
+public class AtendimentoDeixarOCurso extends Atendimento implements Serializable {
+
     @ColunaListavel
     private Long protocolo;
-    
+
     @ColunaListavel
+    @Column(name = "coordenadordiretor")
     private String coordenadorDiretor;
-    
+
     @ColunaListavel
     private String transferencia;
-    
+
     @ColunaListavel
-    @Column(nullable = false)
+    @Column(name = "numeroreprovacoes", nullable = false)
     private int numeroReprovacoes;
-        
+
     @ManyToOne
     @ColunaListavel
+    @JoinColumn(name = "motivo")
     private DeixarOCursoMotivo motivo;
-    
+
     @ManyToOne
     @ColunaListavel
+    @JoinColumn(name = "objetivo")
     private DeixarOCursoObjetivo objetivo;
 
     public AtendimentoDeixarOCurso() {
     }
 
-    public AtendimentoDeixarOCurso(Long id, Long protocolo, String coordenadorDiretor, String transferencia, int numeroReprovacoes, DeixarOCursoMotivo motivo, DeixarOCursoObjetivo objetivo) {
-        this.id = id;
+    public AtendimentoDeixarOCurso(Long protocolo, String coordenadorDiretor, String transferencia, int numeroReprovacoes, DeixarOCursoMotivo motivo, DeixarOCursoObjetivo objetivo) {
         this.protocolo = protocolo;
         this.coordenadorDiretor = coordenadorDiretor;
         this.transferencia = transferencia;
@@ -123,9 +116,7 @@ public class AtendimentoDeixarOCurso implements Serializable {
 
     @Override
     public String toString() {
-        return "AtendimentoDeixarOCurso{" + "id=" + id + ", protocolo=" + protocolo + ", coordenadorDiretor=" + coordenadorDiretor + ", transferencia=" + transferencia + ", numeroReprovacoes=" + numeroReprovacoes + ", motivo=" + motivo + ", objetivo=" + objetivo + '}';
+        return "AtendimentoDeixarOCurso{" + "protocolo=" + protocolo + ", coordenadorDiretor=" + coordenadorDiretor + ", transferencia=" + transferencia + ", numeroReprovacoes=" + numeroReprovacoes + ", motivo=" + motivo + ", objetivo=" + objetivo + '}';
     }
-    
-        
-    
+
 }
--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/atendimento/deixarOCurso/AtendimentoDeixarOCursoService.java
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/atendimento/deixarOCurso/AtendimentoDeixarOCursoService.java
@@ -1,18 +1,31 @@
 package br.unicesumar.escoladeti2015time04.atendimento.deixarOCurso;
 
-import br.unicesumar.escoladeti2015time04.utils.MapRowMapper;
 import br.unicesumar.escoladeti2015time04.utils.service.Service;
-import java.util.List;
-import java.util.Map;
 import javax.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Transactional
 public class AtendimentoDeixarOCursoService extends Service<AtendimentoDeixarOCurso, AtendimentoDeixarOCursoRepository, AtendimentoDeixarOCursoCommandEditar> {
+
+    @Override
+    protected void init() {
+        this.colunasListaveisEntidade.putAll(getMapFieldColunaListavel());
+        this.colunasListaveisEntidade.putAll(getMapFieldColunaListavel(getClassEntity().getSuperclass()));
+        this.idEntidade = getIdEntidade(getClassEntity().getSuperclass());
+        this.selectComColunasListaveis = montarSelectListar();
+        this.selectNumeroRegistros = montarSelectNumeroTotalRegistros(getClassEntity().getSuperclass());
+    }
+
+    @Override
+    protected String montarSelectListar() {
+        String select;
+
+        select = super.montarSelectListar();
+        select += "inner join atendimento a on a.id = atendimentoDeixarOCurso.id";
+
+        return select;
+    }
 
     @Override
     protected Class<AtendimentoDeixarOCurso> getClassEntity() {
