f44060df165b213811bfd8eec98008461ce362ef
renato.nakamura <renato.kenji.nakamura@gmail.com>
2015-05-30 01:11:00 -0300

    #5870 Implementando WSClient

---
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/utils/WSLyceumClient/WSLyceumClientController.java
@@ -0,0 +1,20 @@
+package br.unicesumar.escoladeti2015time04.utils.WSLyceumClient;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("lyceumClient/aluno")
+public class WSLyceumClientController {
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @RequestMapping(value = "/{ra}", method = RequestMethod.GET)
+    public Aluno findByAluno(@PathVariable Long ra) {
+        Aluno aluno = restTemplate.getForObject("http://localhost:9097/lyceum/aluno/" + ra, Aluno.class);
+        return aluno;
+    }
+}
