c57e2f3b8a1619f0c4b2ce4ecde82379fcd71000
gabriel.carvalho <gabrielcapelcavalho@gmail.com>
2015-06-04 07:32:37 -0300

    #5947 Avançado mais um pouco dos perfis de acesso

--- escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
+++ escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
@@ -16,24 +16,33 @@ function controllerListPerfilAcesso($scope, $http, $routeParams, $location) {
 
 
 function controllerFormPerfilUsuario($scope, $http, $routeParams, $location) {
-//    $scope.perfis =
-//            [{perfil: [
-//                        {perfil: "Perfil 1"},
-//                        {perfil: "Perfil 2"},
-//                        {perfil: "Perfil 3"},
-//                        {perfil: "Perfil 4"},
-//                        {perfil: "Perfil 5"}
-//                    ]
-//                }];
-//    
-//    $scope.permissoes =
-//            [{permissao: [
-//                        {nome: "Item Acesso 1"},
-//                        {nome: "Item Acesso 2"},
-//                        {nome: "Item Acesso 3"},
-//                        {nome: "Item Acesso 4"},
-//                        {nome: "Item Acesso 5"}
-//                    ]
-//                }];
-
+    
+    $scope.perfis = [
+        {nomePerfil: 'Perfil de Acesso 1', id:1},  
+        {nomePerfil: 'Perfil de Acesso 2', id:2},
+        {nomePerfil: 'Perfil de Acesso 3', id:3},
+        {nomePerfil: 'Perfil de Acesso 4', id:4}
+    ];
+    $scope.meuPerfil = $scope.perfis[0]; 
+    
+    $scope.adicionarPerfil = function(){
+        $http.post("/perfilUsuario", $scope.perfilUsuario);
+           
+    };
+    
+ 
+//    $scope.nomePerfis = [
+//       
+//    ];
+    
+    $scope.itens =[
+        {nomeItem: 'Item Avulso 1'},
+        {nomeItem: 'Item Avulso 2'}
+    ];
+    $scope.meuItem = $scope.itens[0];
+    $scope.nomeItens = [
+        {nome: 'Item Avulso 3'},  
+        {nome: 'Item Avulso 4'}  
+    ];
+    
 }
