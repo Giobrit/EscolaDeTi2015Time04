151b14c6ae54a3f1c0680d0f47e23bc379d5fc9a
gabriel.carvalho <gabrielcapelcavalho@gmail.com>
2015-06-04 04:01:40 -0300

    #5947 Adicionado mais alguns itens nas telas

--- escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
+++ escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
@@ -4,16 +4,36 @@ AppModule.controller("controllerListPerfilAcesso", controllerListPerfilAcesso);
 
 AppModule.controller("controllerFormPerfilUsuario", controllerFormPerfilUsuario);
 
-function controllerFormPerfilAcesso($scope, $http, $routeParams, $location){
+function controllerFormPerfilAcesso($scope, $http, $routeParams, $location) {
     
-    
-}
 
-function controllerListPerfilAcesso($scope, $http, $routeParams, $location){
-    
 }
 
 
-function controllerFormPerfilUsuario($scope, $http, $routeParams, $location){
-    
-}    
+function controllerListPerfilAcesso($scope, $http, $routeParams, $location) {
+
+}
+
+
+function controllerFormPerfilUsuario($scope, $http, $routeParams, $location) {
+//    $scope.perfis =
+//            [{perfil: [
+//                        {perfil: "Perfil 1"},
+//                        {perfil: "Perfil 2"},
+//                        {perfil: "Perfil 3"},
+//                        {perfil: "Perfil 4"},
+//                        {perfil: "Perfil 5"}
+//                    ]
+//                }];
+//    
+//    $scope.permissoes =
+//            [{permissao: [
+//                        {nome: "Item Acesso 1"},
+//                        {nome: "Item Acesso 2"},
+//                        {nome: "Item Acesso 3"},
+//                        {nome: "Item Acesso 4"},
+//                        {nome: "Item Acesso 5"}
+//                    ]
+//                }];
+
+}
