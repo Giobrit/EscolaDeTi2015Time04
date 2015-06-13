048f4e892e572dfc2303d1f78dce40939c9c7c21
Rodrigo Ferreira <roferreira122@gmail.com>
2015-06-13 17:41:50 -0300

    issue#6070 Alterações para exibir data e hora na listagem, concluindo listagem de AtendimentoDeixarOCurso

--- escoladeti2015time04/src/main/webapp/view/atendimentoDeixarOCurso/controllerAtendimentoDeixarOCurso.js
+++ escoladeti2015time04/src/main/webapp/view/atendimentoDeixarOCurso/controllerAtendimentoDeixarOCurso.js
@@ -3,27 +3,28 @@ function controllerListAtendimentoDeixarOCurso($scope, $http, growl) {
 
     $scope.paginaAtual = 1;
     $scope.numeroItensPorPagina = 8;
-    $scope.colunaOrdenacao = "nomeAluno";
+    colunaOrdenacao = "protocolo";
+    $scope.labelOrdenacao = "Protocolo";
     var ordenacaoCrescente = true;
 
     $scope.colunas =
             [
-                {label: "Protocolo", nome: "protocolo", checked: false},
-                {label: "Data", nome: "data", checked: false},
-                {label: "Hora", nome: "hora", checked: false},
-                {label: "RA", nome: "ra", checked: false},
-                {label: "Nome Aluno", nome: "nomeAluno", checked: false},
-                {label: "Curso", nome: "curso", checked: false},
-                {label: "Série", nome: "serieSemestre", checked: false},
-                {label: "Turno", nome: "turno", checked: false},
-                {label: "Bolsa", nome: "bolsaFinanciamento", checked: false},
-                {label: "Reprovações", nome: "numeroReprovacoes", checked: false},
-                {label: "Matriculado", nome: "matriculado", checked: true},
-                {label: "Transferência", nome: "transferencia", checked: true},
-                {label: "Coordenador", nome: "coordenadorDiretor", checked: true},
-                {label: "Objetivo", nome: "objetivo", checked: true},
-                {label: "Motivo", nome: "motivo", checked: true},
-                {label: "Descrição", nome: "descricaoPublica", checked: true}
+                {label: "Protocolo", colunaOrdenacao: "protocolo", propriedadeItem: "protocolo", checked: false},
+                {label: "Data", colunaOrdenacao: "data", propriedadeItem: "data", checked: true},
+                {label: "Hora", colunaOrdenacao: "data", propriedadeItem: "hora", checked: true},
+                {label: "RA", colunaOrdenacao: "ra", propriedadeItem: "ra", checked: false},
+                {label: "Nome Aluno", colunaOrdenacao: "colunaOrdenacaoAluno", propriedadeItem: "colunaOrdenacaoAluno", checked: false},
+                {label: "Curso", colunaOrdenacao: "curso", propriedadeItem: "curso", checked: false},
+                {label: "Série", colunaOrdenacao: "serieSemestre", propriedadeItem: "serieSemestre", checked: false},
+                {label: "Turno", colunaOrdenacao: "turno", propriedadeItem: "turno", checked: false},
+                {label: "Bolsa", colunaOrdenacao: "bolsaFinanciamento", propriedadeItem: "bolsaFinanciamento", checked: false},
+                {label: "Reprovações", colunaOrdenacao: "numeroReprovacoes", propriedadeItem: "numeroReprovacoes", checked: false},
+                {label: "Matriculado", colunaOrdenacao: "matriculado", propriedadeItem: "matriculado", checked: true},
+                {label: "Transferência", colunaOrdenacao: "transferencia", propriedadeItem: "transferencia", checked: true},
+                {label: "Coordenador", colunaOrdenacao: "coordenadorDiretor", propriedadeItem: "coordenadorDiretor", checked: true},
+                {label: "Objetivo", colunaOrdenacao: "objetivo", propriedadeItem: "objetivo", checked: true},
+                {label: "Motivo", colunaOrdenacao: "motivo", propriedadeItem: "motivo", checked: true},
+                {label: "Descrição", colunaOrdenacao: "descricaoPublica", propriedadeItem: "descricaoPublica", checked: true}
             ];
 
     $scope.init = function () {
@@ -34,24 +35,36 @@ function controllerListAtendimentoDeixarOCurso($scope, $http, growl) {
         var requisicaoListagem = new RequisicaoListagem();
         requisicaoListagem.numeroItens = $scope.numeroItensPorPagina;
         requisicaoListagem.paginaAtual = $scope.paginaAtual;
-        requisicaoListagem.colunaOrdenacao = $scope.colunaOrdenacao;
+        requisicaoListagem.colunaOrdenacao = colunaOrdenacao;
         requisicaoListagem.ordenacaoCrescente = $scope.ordenacaoCrescente;
         requisicaoListagem.valorFiltragem = $scope.pesquisa;
         requisicaoListagem.colunasVisiveis = colunasVisiveis();
         $http.post("atendimento/deixarOCurso/listar", requisicaoListagem).success(onSuccess).error(onError);
         function onSuccess(data) {
+            setDataHora(data.itens);
             $scope.atendimentos = data.itens;
             $scope.totalRegistros = data.numeroTotalRegistros;
         }
+
+        function setDataHora(itens) {
+            itens.forEach(paraCada);
+
+            function paraCada(item) {
+                item.hora = timestampParaHora(item.data);
+
+                item.data = timestampParaData(item.data);
+            }
+        }
     };
     $scope.alterarOrdenacao = function (coluna) {
-        if ($scope.colunaOrdenacao === coluna) {
+        if (colunaOrdenacao === coluna.colunaOrdenacao) {
             $scope.ordenacaoCrescente = !$scope.ordenacaoCrescente;
         } else {
             $scope.ordenacaoCrescente = true;
         }
 
-        $scope.colunaOrdenacao = coluna;
+        colunaOrdenacao = coluna.colunaOrdenacao;
+        $scope.labelOrdenacao = coluna.label;
         $scope.listar();
     };
     $scope.alterarPagina = function () {
@@ -73,7 +86,7 @@ function controllerListAtendimentoDeixarOCurso($scope, $http, growl) {
 
         function paraCadaElemento(coluna) {
             if (coluna.checked) {
-                colunasVisiveis.push(coluna.nome);
+                colunasVisiveis.push(coluna.propriedadeItem);
             }
         }
 
