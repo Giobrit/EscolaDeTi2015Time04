9a7c9f0426f4cbae1b80c32ff638dc2a15cb23b9
giovanni.dias <gio_br@live.com>
2015-06-23 16:37:23 -0300

    #6067 completando a parte de Perfil De Acesso

--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/perfilacesso/PerfilAcessoService.java
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/perfilacesso/PerfilAcessoService.java
@@ -3,6 +3,7 @@ package br.unicesumar.escoladeti2015time04.perfilacesso;
 import br.unicesumar.escoladeti2015time04.itemAcesso.ItemAcesso;
 import br.unicesumar.escoladeti2015time04.itemAcesso.ItemAcessoService;
 import br.unicesumar.escoladeti2015time04.utils.MapRowMapper;
+import br.unicesumar.escoladeti2015time04.utils.service.Service;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -10,29 +11,25 @@ import java.util.Set;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Transactional
-public class PerfilAcessoService {
+public class PerfilAcessoService extends Service<PerfilAcesso, PerfilAcessoRepository, PerfilAcessoCommandEditar> {
 
-    @Autowired
-    private PerfilAcessoRepository perfilRepository;
     @Autowired
     private ItemAcessoService itemAcessoService;
-    @Autowired
-    private NamedParameterJdbcTemplate jdbcTemplate;
 
-    public void salvar(CriarPerfilAcessoCommand command) {
-        if (existePerfil(command.getNome())) {
-            throw new IllegalArgumentException("Já existe Perfil de acesso com o mesmo nome.");
-        }
-        if (command.getNome().isEmpty()) {
+    public void criar(PerfilAcessoCommandInserir command) {
+        if (command.getNome() == null || command.getNome().isEmpty()) {
             throw new IllegalArgumentException("É obrigatório informar um nome para o perfil.");
         }
 
-        if (command.getConjuntoItemAcessoIds().size() == 0) {
+        if (existePerfil(command.getNome())) {
+            throw new IllegalArgumentException("Já existe Perfil de acesso com o mesmo nome.");
+        }
+
+        if (command.getConjuntoItemAcessoIds().isEmpty()) {
             throw new IllegalArgumentException("O perfil deve possuir ao menos um item de acesso.");
         }
 
@@ -40,69 +37,55 @@ public class PerfilAcessoService {
         lista.addAll(itemAcessoService.findAllSet(command.getConjuntoItemAcessoIds()));
 
         PerfilAcesso perfil = new PerfilAcesso(command.getNome());
-        perfil.setConjuntoItemAcesso(lista);
+        perfil.setItensDeAcesso(lista);
 
-        perfilRepository.save(perfil);
+        repository.save(perfil);
     }
 
-    public void editar(EditarPerfilAcessoCommand command) {
-        if (command.getNome().isEmpty()) {
+    public void editar(PerfilAcessoCommandEditar command) {
+        if (command.getNome() == null || command.getNome().isEmpty()) {
             throw new IllegalArgumentException("É obrigatório informar um nome para o perfil.");
         }
 
-        if (command.getConjuntoItemAcessoIds().size() == 0) {
+        if (command.getItensDeAcesso().isEmpty()) {
             throw new IllegalArgumentException("O perfil deve possuir ao menos um item de acesso.");
         }
 
         Set<ItemAcesso> lista = new HashSet<>();
-        lista.addAll(itemAcessoService.findAllSet(command.getConjuntoItemAcessoIds()));
+        lista.addAll(itemAcessoService.findAllSet(command.getItensDeAcesso()));
 
         PerfilAcesso perfil = new PerfilAcesso(command.getId(), command.getNome());
-        perfil.setConjuntoItemAcesso(lista);
-        perfilRepository.save(perfil);
+        perfil.setItensDeAcesso(lista);
+        repository.save(perfil);
+    }
+
+    @Override
+    public Map<String, Object> localizar(Long id) {
+        final Map<String, Object> perfilAcesso = super.localizar(id);
+
+        final String query = ""
+                + " SELECT"
+                + " id"
+                + " from itemAcesso ia"
+                + " inner join perfilacesso_itemacesso pfia on iditemacesso = ia.id "
+                + " where "
+                + " pfia.idperfilacesso = :idPerfilAcesso";
+
+        final MapSqlParameterSource parans = new MapSqlParameterSource();
+        parans.addValue("idPerfilAcesso", id);
+
+        final List<Long> itensDeAcesso = jdbcTemplate.queryForList(query, parans, Long.class);
+        
+        perfilAcesso.put("itensDeAcesso", itensDeAcesso);
+        
+        return perfilAcesso; //To change body of generated methods, choose Tools | Templates.
     }
 
     public void remover(Long id) {
         /*Precisa verificar se existe este perfil em algum usuario, caso "sim" dar a mensagem que possui 
          cadastro com este perfil(Aguardando vinculo com perfil-usuario)*/
 
-        perfilRepository.delete(id);
-    }
-
-    public List<Map<String, Object>> listarPerfilAcesso() {
-        List<Map<String, Object>> listaDePerfil;
-        listaDePerfil = jdbcTemplate.query(""
-                + "select "
-                + "p.nome,"
-                + "p.id "
-                + "from perfilacesso p", new MapSqlParameterSource(), new MapRowMapper());
-
-        return listaDePerfil;
-    }
-
-    public Map<String, Object> findById(Long id) {
-        final MapSqlParameterSource params = new MapSqlParameterSource();
-        params.addValue("id", id);
-
-        List<Map<String, Object>> perfis = jdbcTemplate.query(""
-                + "select p.id,"
-                + "p.nome "
-                + "from perfilacesso p "
-                + "where p.id = :id", params, new MapRowMapper());
-
-        if (perfis.isEmpty()) {
-            throw new IllegalArgumentException("Perfil não existe!");
-        }
-
-        Map<String, Object> perfil = perfis.get(0);
-
-        List<Map<String, Object>> itensAcessoIds = jdbcTemplate.query("select ip.itemacesso_id as id "
-                + "from itemacesso_perfilacesso ip "
-                + "where perfilacesso_id = :id", params, new MapRowMapper());
-
-        perfil.put("itemAcesso", itemAcessoService.findAll(itensAcessoIds));
-
-        return perfil;
+        repository.delete(id);
     }
 
     public Boolean existePerfil(String nome) {
@@ -119,6 +102,11 @@ public class PerfilAcessoService {
             return true;
         }
         return false;
+    }
+
+    @Override
+    protected Class<PerfilAcesso> getClassEntity() {
+        return PerfilAcesso.class;
     }
 
 }
