5180cb65a55abdc33a6258de250ad5968139847a
giovanni.dias <gio_br@live.com>
2015-07-23 17:18:01 -0300

    #6066 atualizando para o 1.4

--- escoladeti2015time04/src/main/webapp/bibliotecas/js/router.js
+++ escoladeti2015time04/src/main/webapp/bibliotecas/js/router.js
@@ -1,86 +1,105 @@
-AppModule.config(function ($routeProvider, $locationProvider) {
+(function () {
+    AppModule.config(function ($routeProvider, $locationProvider) {
 
-    $routeProvider.when('/PerfilAcesso/form', {
-        templateUrl: 'view/perfilAcesso/formPerfilAcesso.html',
-        controller: 'controllerFormPerfilAcesso',
-        resolve: {
-            logado: validacaoLogin
-        }
-    }).when('/PerfilAcesso/list', {
-        templateUrl: 'view/perfilAcesso/listPerfilAcesso.html',
-        controller: 'controllerListPerfilAcesso'
-    }).when('/PerfilAcesso/formPerfilUsuario', {
-        templateUrl: 'view/perfilAcesso/formPerfilUsuario.html',
-        controller: 'controllerFormPerfilUsuario'
-    }).when('/PerfilAcesso/edit/:id', {
-        templateUrl: 'view/PerfilAcesso.html',
-        controller: 'PerfilAcessoFormController'
-    }).when('/PerfilAcesso/lista', {
-        templateUrl: 'view/ListaPerfilAcesso.html',
-        controller: 'PerfilAcessoFormController'
-    }).when('/Usuario/form', {
-        templateUrl: 'view/usuario/cadastroUsuario.html',
-        controller: 'controllerFormUsuario'
-    }).when('/Usuario/form/:id', {
-        templateUrl: 'view/usuario/cadastroUsuario.html',
-        controller: 'controllerFormUsuario'
-    }).when('/Usuario/list', {
-        templateUrl: 'view/usuario/ListagemUsuario.html',
-        controller: 'controllerListUsuario'
-    }).when('/Usuario/login', {
-        templateUrl: 'view/login/login.html',
-        controller: ''
-    }).when('/Usuario/form/alterarSenha/:id', {
-        templateUrl: 'view/usuario/alterarSenhaUsuario.html',
-        controller: 'controllerFormUsuario'
-    }).when('/AtendimentoDeixarOCurso/Objetivo/form', {
-        templateUrl: 'view/atendimentoDeixarOCurso/objetivo/cadastroObjetivo.html',
-        controller: 'controllerFormObjetivoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/Objetivo/form/:id', {
-        templateUrl: 'view/atendimentoDeixarOCurso/objetivo/cadastroObjetivo.html',
-        controller: 'controllerFormObjetivoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/Objetivo/list', {
-        templateUrl: 'view/atendimentoDeixarOCurso/objetivo/listagemObjetivo.html',
-        controller: 'controllerListObjetivoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/Motivo/form', {
-        templateUrl: 'view/atendimentoDeixarOCurso/motivo/cadastroMotivo.html',
-        controller: 'controllerFormMotivoAtendimentoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/Motivo/list', {
-        templateUrl: 'view/atendimentoDeixarOCurso/motivo/listagemMotivo.html',
-        controller: 'controllerListMotivoAtendimentoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/Motivo/form/:id', {
-        templateUrl: 'view/atendimentoDeixarOCurso/motivo/cadastroMotivo.html',
-        controller: 'controllerFormMotivoAtendimentoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/form', {
-        templateUrl: 'view/atendimentoDeixarOCurso/formAtendimentoDeixarOCurso.html',
-        controller: 'controllerFormAtendimentoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/form/:id', {
-        templateUrl: 'view/atendimentoDeixarOCurso/formAtendimentoDeixarOCurso.html',
-        controller: 'controllerFormAtendimentoDeixarOCurso'
-    }).when('/AtendimentoDeixarOCurso/list', {
-        templateUrl: 'view/atendimentoDeixarOCurso/ListAtendimentoDeixarOCurso.html',
-        controller: 'controllerListAtendimentoDeixarOCurso'
-    }).when('/AtendimentoPreventivo/Motivo/form', {
-        templateUrl: 'view/atendimentoPreventivo/motivo/cadastroMotivo.html',
-        controller: 'controllerFormMotivoAtendimentoPreventivo'
-    }).when('/AtendimentoPreventivo/Motivo/form/:id', {
-        templateUrl: 'view/atendimentoPreventivo/motivo/cadastroMotivo.html',
-        controller: 'controllerFormMotivoAtendimentoPreventivo'
-    }).when('/AtendimentoPreventivo/Motivo/list', {
-        templateUrl: 'view/atendimentoPreventivo/motivo/listagemMotivo.html',
-        controller: 'controllerListMotivoAtendimentoPreventivo'
-    }).otherwise('/', {
-        templateUrl: 'view/Home.html'
+        adicionarRota($routeProvider, '/login', 'view/login/login.html', 'controllerTelaLogin');
+        adicionarRota($routeProvider, '/', 'view/Home.html');
+        //Rotas PerfilAcesso
+        adicionarRota($routeProvider, '/PerfilAcesso/form', 'view/perfilAcesso/formPerfilAcesso.html', 'controllerFormPerfilAcesso');
+        adicionarRota($routeProvider, '/PerfilAcesso/list', 'view/perfilAcesso/listPerfilAcesso.html', 'controllerListPerfilAcesso');
+        adicionarRota($routeProvider, '/PerfilAcesso/edit/:id', 'view/PerfilAcesso.html', 'PerfilAcessoFormController');
+        //Rotas PerfilUsuario
+        adicionarRota($routeProvider, '/PerfilUsuario/form', 'view/perfilAcesso/formPerfilUsuario.html', 'controllerFormPerfilUsuario');
+        //Rotas Usuario
+        adicionarRota($routeProvider, '/Usuario/form', 'view/usuario/cadastroUsuario.html', 'controllerFormUsuario');
+        adicionarRota($routeProvider, '/Usuario/form/:id', 'view/usuario/cadastroUsuario.html', 'controllerFormUsuario');
+        adicionarRota($routeProvider, '/Usuario/list', 'view/usuario/ListagemUsuario.html', 'controllerListUsuario');
+        //Rotas Atendimento Deixar O Curso
+        adicionarRota($routeProvider, '/AtendimentoDeixarOCurso/form', 'view/atendimentoDeixarOCurso/formAtendimentoDeixarOCurso.html', 'controllerFormAtendimentoDeixarOCurso');
+        adicionarRota($routeProvider, '/AtendimentoDeixarOCurso/form/:id', 'view/atendimentoDeixarOCurso/formAtendimentoDeixarOCurso.html', 'controllerFormAtendimentoDeixarOCurso');
+        adicionarRota($routeProvider, '/AtendimentoDeixarOCurso/list', 'view/atendimentoDeixarOCurso/ListAtendimentoDeixarOCurso.html', 'controllerListAtendimentoDeixarOCurso');
+        //Rotas
+
+//        }).when('/Usuario/login', {
+//            templateUrl: 'view/login/login.html',
+//            controller: ''
+//        }).when('/Usuario/form/alterarSenha/:id', {
+//            templateUrl: 'view/usuario/alterarSenhaUsuario.html',
+//            controller: 'controllerFormUsuario'
+//        }).when('/AtendimentoDeixarOCurso/Objetivo/form', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/objetivo/cadastroObjetivo.html',
+//            controller: 'controllerFormObjetivoDeixarOCurso'
+//        }).when('/AtendimentoDeixarOCurso/Objetivo/form/:id', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/objetivo/cadastroObjetivo.html',
+//            controller: 'controllerFormObjetivoDeixarOCurso'
+//        }).when('/AtendimentoDeixarOCurso/Objetivo/list', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/objetivo/listagemObjetivo.html',
+//            controller: 'controllerListObjetivoDeixarOCurso'
+//        }).when('/AtendimentoDeixarOCurso/Motivo/form', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/motivo/cadastroMotivo.html',
+//            controller: 'controllerFormMotivoAtendimentoDeixarOCurso'
+//        }).when('/AtendimentoDeixarOCurso/Motivo/list', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/motivo/listagemMotivo.html',
+//            controller: 'controllerListMotivoAtendimentoDeixarOCurso'
+//        }).when('/AtendimentoDeixarOCurso/Motivo/form/:id', {
+//            templateUrl: 'view/atendimentoDeixarOCurso/motivo/cadastroMotivo.html',
+//            controller: 'controllerFormMotivoAtendimentoDeixarOCurso'
+//        }).when('/AtendimentoPreventivo/Motivo/form', {
+//            templateUrl: 'view/atendimentoPreventivo/motivo/cadastroMotivo.html',
+//            controller: 'controllerFormMotivoAtendimentoPreventivo'
+//        }).when('/AtendimentoPreventivo/Motivo/form/:id', {
+//            templateUrl: 'view/atendimentoPreventivo/motivo/cadastroMotivo.html',
+//            controller: 'controllerFormMotivoAtendimentoPreventivo'
+//        }).when('/AtendimentoPreventivo/Motivo/list', {
+//            templateUrl: 'view/atendimentoPreventivo/motivo/listagemMotivo.html',
+//            controller: 'controllerListMotivoAtendimentoPreventivo'
+//        });
+
+        //verificar se é possível separar as rotas em módulos
+
+        $locationProvider.html5Mode(false);
+
     });
 
-    //verificar se é possível separar as rotas em módulos
+    function adicionarRota(routeProvider, rota, urlTemplate, controllerName) {
+        routeProvider.when(rota, {
+            templateUrl: urlTemplate,
+            controller: controllerName,
+            resolve: {
+                login: validacaoLogin
+            }
+        });
+    }
 
-    $locationProvider.html5Mode(false);
-});
+    function validacaoLogin($q, $cookies, $location, growl) {
+        var now = new Date();
+        $cookies.put('login', 'o cabra tah logado', {
+            expires: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 1)
+        });
+        var deferred = $q.defer();
+        var cookie = $cookies.get('login');
+//    var cookie = $cookies.remove('login');//('login');
 
+//        console.log(cookie);
+        if (!cookie) {
+            deferred.resolve();
+            $location.path("/login");
+        } else {
+            var now = new Date();
+            $cookies.put('login', 'o_cabra_tah_logado', {
+                expires: new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 30)
+            });
+            if (!validarPermissoes(deferred)) {
+                growl.warning("Acesso Negado");
+                $location.path("/");
+            }
+        }
 
-function validacaoLogin($q, $timeout) {
-    console.log("weiygfher9iug");
-    var deferred = $q.defe();
-    return deferred.promise;
-}
+        return deferred.promise;
+    }
+
+    function validarPermissoes(deferred) {
+        deferred.resolve();
+
+        return true;
+    }
+}());
--- escoladeti2015time04/src/main/webapp/view/login/controllerLogin.js
+++ escoladeti2015time04/src/main/webapp/view/login/controllerLogin.js
@@ -1,30 +1,26 @@
-AppModule.controller("controllerLogin", controllerLogin);
-aaa = 1;
-function controllerLogin($scope, $http, $location) {
+AppModule.controller("controllerTelaLogin", controllerDoLogin);
+
+function controllerDoLogin($scope, $http, $location) {
 
     $scope.init = function () {
         limparTela();
     };
 
     $scope.logar = function () {
-        $http.post("/usuario/logar", $scope.login).success(onSuccess).error(onError);
-        
+        $http.post("/usuario/logar", $scope.login).success(onSuccess).error($scope.onError);
+
         function onSuccess(data) {
             if (data) {
                 alert("você logou");
                 window.location($location.host() + ":" + $location.port() + "/#/");
                 //$location.absUrl($location.host() + ":" + $location.port() + "/#/");
             } else {
-                alert("Login ou sneha incorretos!");                
+                alert("Login ou sneha incorretos!");
             }
         }
-    }
+    };
 
     function limparTela() {
         $scope.login = {};
-    }
-
-    function onError(data) {
-        alert(JSON.stringify(data));
     }
 }
