754ab224e056dff58a3b43831e4812496f3bfb68
giovanni.dias <gio_br@live.com>
2015-07-25 06:31:18 -0300

    #6066 continuando...

--- escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
+++ escoladeti2015time04/src/main/webapp/view/perfilAcesso/controllerPerfilAcesso.js
@@ -31,7 +31,7 @@ function controllerFormPerfilAcesso($scope, $http, $location, $routeParams, grow
             }
         }
 
-            
+
         if ($scope.editando) {
             $http.put("/perfilAcesso", $scope.perfilDeAcesso).success(onSuccess).error($scope.onError);
         } else {
@@ -49,7 +49,7 @@ function controllerFormPerfilAcesso($scope, $http, $location, $routeParams, grow
         function onSuccess(data) {
             $scope.perfilDeAcesso = data;
             $scope.perfilDeAcesso.itensDeAcesso.forEach(percorreIdsQueElePossui);
-            
+
             function percorreIdsQueElePossui(idItem) {
                 $scope.itensAcesso.forEach(percorreItensAcesso);
                 function percorreItensAcesso(itemAcesso) {
@@ -131,37 +131,16 @@ function controllerFormPerfilAcesso($scope, $http, $location, $routeParams, grow
 
 }
 
-function controllerListPerfilAcesso($scope, $http, $routeParams, $location) {
+function controllerListPerfilAcesso($scope, $http, $location, $routeParams, growl) {
 
     $scope.init = function () {
         $scope.listar();
     };
-    
+
     $scope.paginaAtual = 1;
     $scope.numeroItensPorPagina = 8;
-    var colunaOrdenacao = "protocolo";
-    $scope.labelOrdenacao = "Protocolo";
-    var ordenacaoCrescente = true;
-
-    $scope.colunas =
-            [
-                {label: "Protocolo", colunaOrdenacao: "protocolo", propriedadeItem: "protocolo", checked: false},
-                {label: "Data", colunaOrdenacao: "data", propriedadeItem: "data", checked: true},
-                {label: "Hora", colunaOrdenacao: "data", propriedadeItem: "hora", checked: true},
-                {label: "RA", colunaOrdenacao: "ra", propriedadeItem: "ra", checked: false},
-                {label: "Nome Aluno", colunaOrdenacao: "nomeAluno", propriedadeItem: "nomeAluno", checked: true},
-                {label: "Curso", colunaOrdenacao: "curso", propriedadeItem: "curso", checked: false},
-                {label: "Série", colunaOrdenacao: "serieSemestre", propriedadeItem: "serieSemestre", checked: false},
-                {label: "Turno", colunaOrdenacao: "turno", propriedadeItem: "turno", checked: false},
-                {label: "Bolsa", colunaOrdenacao: "bolsaFinanciamento", propriedadeItem: "bolsaFinanciamento", checked: false},
-                {label: "Reprovações", colunaOrdenacao: "numeroReprovacoes", propriedadeItem: "numeroReprovacoes", checked: false},
-                {label: "Matriculado", colunaOrdenacao: "matriculado", propriedadeItem: "matriculado", checked: true},
-                {label: "Transferência", colunaOrdenacao: "transferencia", propriedadeItem: "transferencia", checked: true},
-                {label: "Coordenador", colunaOrdenacao: "coordenadorDiretor", propriedadeItem: "coordenadorDiretor", checked: true},
-                {label: "Objetivo", colunaOrdenacao: "objetivo", propriedadeItem: "objetivo", checked: true},
-                {label: "Motivo", colunaOrdenacao: "motivo", propriedadeItem: "motivo", checked: true},
-                {label: "Descrição", colunaOrdenacao: "descricaoPublica", propriedadeItem: "descricaoPublica", checked: true}
-            ];
+    var colunaOrdenacao = "nome";
+    $scope.labelOrdenacao = "Nome";
 
     $scope.init = function () {
         $scope.listar();
@@ -174,24 +153,15 @@ function controllerListPerfilAcesso($scope, $http, $routeParams, $location) {
         requisicaoListagem.colunaOrdenacao = colunaOrdenacao;
         requisicaoListagem.ordenacaoCrescente = $scope.ordenacaoCrescente;
         requisicaoListagem.valorFiltragem = $scope.pesquisa;
-        requisicaoListagem.colunasVisiveis = colunasVisiveis();
-        $http.post("perfilAcesso/listar", requisicaoListagem).success(onSuccess).error(onError);
+
+        $http.post("perfilAcesso/listar", requisicaoListagem).success(onSuccess).error($scope.onError);
         function onSuccess(data) {
-            setDataHora(data.itens);
-            $scope.atendimentos = data.itens;
+            $scope.perfisAcesso = data.itens;
+            console.log(data.itens);
             $scope.totalRegistros = data.numeroTotalRegistros;
         }
-
-        function setDataHora(itens) {
-            itens.forEach(paraCada);
-
-            function paraCada(item) {
-                item.hora = timestampParaHora(item.data);
-
-                item.data = timestampParaData(item.data);
-            }
-        }
     };
+    
     $scope.alterarOrdenacao = function (coluna) {
         if (colunaOrdenacao === coluna.colunaOrdenacao) {
             $scope.ordenacaoCrescente = !$scope.ordenacaoCrescente;
@@ -203,32 +173,11 @@ function controllerListPerfilAcesso($scope, $http, $routeParams, $location) {
         $scope.labelOrdenacao = coluna.label;
         $scope.listar();
     };
+
     $scope.alterarPagina = function () {
         $scope.listar();
     };
-    function onError(data) {
-        console.log(JSON.stringify(data));
-        growl.error(JSON.stringify(data));
-    }
 
-    $scope.alterarCheckbox = function (coluna) {
-        coluna.checked = !coluna.checked;
-        $scope.listar();
-    };
-
-    function colunasVisiveis() {
-        var colunasVisiveis = [];
-
-        $scope.colunas.forEach(paraCadaElemento);
-
-        function paraCadaElemento(coluna) {
-            if (coluna.checked) {
-                colunasVisiveis.push(coluna.propriedadeItem);
-            }
-        }
-
-        return colunasVisiveis;
-    }
 }
 
 
