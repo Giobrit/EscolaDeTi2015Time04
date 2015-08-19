493b383a50c4155fb54773efac86dd5b82657be0
filipe.maldonado <filipe.maldonado89@gmail.com>
2015-08-19 20:55:33 -0300

    #6523 - Implementando metodos

--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/relatorio/centro/RelatorioCentroController.java
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/relatorio/centro/RelatorioCentroController.java
@@ -2,20 +2,21 @@ package br.unicesumar.escoladeti2015time04.relatorio.centro;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/relatorio/resumo")
+@RequestMapping("/relatorio/porcentro")
 public class RelatorioCentroController {
     
     @Autowired
     private RelatorioCentroService service;
     
-    @RequestMapping(value = "/relatorioCentroCursoAtendimentos", method = RequestMethod.GET)    
-    public Map<String,Object> listarCentroCursoAtendimento(){
-        return service.getCentroCursoAtendimento(null);
+    @RequestMapping(value = "/relatorioCentroCursoAtendimentos/{centro}", method = RequestMethod.GET)    
+    public Map<String,Object> listarCentroCursoAtendimento(@PathVariable String centro){
+        return service.getCentroCursoAtendimento(centro);
     }
     
     @RequestMapping(value = "/relatorioCentroCursoAlunos", method = RequestMethod.GET)    
--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/relatorio/centro/RelatorioCentroService.java
+++ escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/relatorio/centro/RelatorioCentroService.java
@@ -1,20 +1,32 @@
 package br.unicesumar.escoladeti2015time04.relatorio.centro;
 
+import br.unicesumar.escoladeti2015time04.utils.MapRowMapper;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @Transactional
 public class RelatorioCentroService {
-    
-    public Map<String,Object> getCentroCursoAtendimento(String centro) {
-        String  queryTotalAtenditmentos = "select count(*) as atendimentos"
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    public Map<String, Object> getCentroCursoAtendimento(String centro) {
+        MapSqlParameterSource parans = new MapSqlParameterSource();
+        parans.addValue(":centro", centro);
+
+        String queryTotalAtenditmentos = "select count(*) as atendimentos"
                 + "from antedimento  att "
                 + "inner join atendimentodeixarocurso atdc on att.id = atdc.id "
                 + "where centro = :centro"
                 + "group by att.centro ";
-        
+
         String queryTrancamentosCancelamentosTransferencias = "select count(*)"
                 + "as trancamentoscancelamentostransferencias "
                 + "from atendimento att "
@@ -23,43 +35,62 @@ public class RelatorioCentroService {
                 + "where dco.descricao in('Trancamento', 'Cancelamento', 'Tranferência') "
                 + "and  att.centro = :centro "
                 + "group by att.centro";
-        
+
         String queryPermanencias = "select count(*) as permanencias "
                 + "from atendimento att "
                 + "inner join atendimentodeixarocurso atdc on atdc.id = att.id "
                 + "inner join deixarocursoobjetivo dco on dco.id = atdc.objetivo "
                 + "where descricao = 'Permanencia' "
                 + "and att.centro = :centro "
-                + "group by att.centro";   
-                               
-        return null;    
+                + "group by att.centro";
+        Map<String, Object> retorno = new HashMap<String, Object>();
+
+        List<Map<String, Object>> atendimentos = jdbcTemplate.query(queryTotalAtenditmentos, new MapRowMapper());
+        retorno.put("atendimentos", atendimentos);        
+        List<Map<String, Object>> trancamentosCancelamentosTransferencias = jdbcTemplate.query(queryTrancamentosCancelamentosTransferencias, new MapRowMapper());        
+        retorno.put("trancamentosCancelamentosTransferencias", trancamentosCancelamentosTransferencias);        
+        List<Map<String, Object>> permanencias = jdbcTemplate.query(queryPermanencias, new MapRowMapper());
+        retorno.put("permanencias", permanencias);        
+        
+        System.out.print(retorno.toString());
+
+        return null;
     }
 
-    public Map<String,Object> getCentroCursoAlunos() {
-        
+    public Map<String, Object> getCentroCursoAlunos() {
+
         return null;
     }
-    
-    public Map<String,Object> getCentroMotivosPorCurso(){
-        String queryMotivosPorCurso = "select count(*) as motivos "
-                + "from atendimento att "
-                + "inner join atendimentodeixarocurso atdc on att.id = atdc.id "
-                + "inner join deixarcursoobjetivo dco on dco.motivo = att.id "
-                + "where att.centro = :centro "
-                + "and att.curso = :curso "
-                + "group by att.centro, atdc.motivo ";
-                       
+
+    public Map<String, Object> getCentroMotivosPorCurso() {
+        String queryMotivosPorCurso = "select ";
+
+
+
+
+        //String queryMotivosPorCurso = "select count(*) as motivos "
+        //       + "from atendimento att "
+        //        + "inner join atendimentodeixarocurso atdc on att.id = atdc.id "
+        //        + "inner join deixarcursoobjetivo dco on dco.motivo = att.id "
+        //        + "where att.centro = :centro "
+        //        + "and att.curso = :curso "
+        //        + "group by att.centro, atdc.motivo ";
+
         return null;
     }
-    
-    public Map<String,Object> getCentroResumoMotivos(){
-       String queryResumoMotivos = "select count (*) as motivos "
-               + "from atendimento att "
-               + "inner join atendimentodeixarocurso atdc on att.id = atdc.id "
-               + "inner join deixarocursoobjetivo dco on dco.motivo = att.id "
-               + "where att.centro = :centro "
-               + "group by atdc.motivo";
-        
+
+    public Map<String, Object> getCentroResumoMotivos() {
+        String queryResumoMotivos = "select ";
+
+
+
+        //String queryResumoMotivos = "select count (*) as motivos "
+        //        + "from atendimento att "
+        //        + "inner join atendimentodeixarocurso atdc on att.id = atdc.id "
+        //        + "inner join deixarocursoobjetivo dco on dco.motivo = att.id "
+        //        + "where att.centro = :centro "
+        //        + "group by atdc.motivo";
+
         return null;
     }
 }
