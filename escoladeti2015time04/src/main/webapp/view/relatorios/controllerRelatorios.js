3f81b36cb8f8adfbfb2384dd7ebb26c6d3571565
gabriel.carvalho <gabrielcapelcavalho@gmail.com>
2015-08-20 20:10:20 -0300

    #6524 Finalizado a etapa de criação dos relatorios para os relatorios resumidos

--- escoladeti2015time04/src/main/webapp/view/relatorios/controllerRelatorios.js
+++ escoladeti2015time04/src/main/webapp/view/relatorios/controllerRelatorios.js
@@ -157,6 +157,174 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                 });
             });
         });
+        $(function () {
+
+            $(document).ready(function () {
+
+                // Build the chart
+                $('#pie2').highcharts({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Resumo motivos'
+                    },
+                    credits:{
+                        enabled: false
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    series: [{
+                            name: "Brands",
+                            colorByPoint: true,
+                            data: [{
+                                    name: "Aprendizagem",
+                                    y: 7
+                                }, {
+                                    name: "Distância",
+                                    y: 14
+                                }, 
+                                {
+                                    name: "Doença",
+                                    y: 25
+                                },
+                                {
+                                    name: "Financeiro",
+                                    y: 286
+                                },
+                                {
+                                    name: "Frequência",
+                                    y: 1
+                                },
+                                {
+                                    name: "Gravides",
+                                    y: 7
+                                },
+                                {
+                                    name: "Mudança de cidade",
+                                    y: 127
+                                },
+                                {
+                                    name: "Não indentificação com o curso",
+                                    y: 76
+                                },
+                                {
+                                    name: "Notas baixas",
+                                    y: 11
+                                },
+                                {
+                                    name: "Outros",
+                                    y: 177
+                                },
+                                {
+                                    name: "Trabalho",
+                                    y: 64
+                                },{
+                                    name: "Transferencia para outra instituição",
+                                    y: 213
+                                }]
+                        }]
+                });
+            });
+        });
+        $(function () {
+
+            $(document).ready(function () {
+
+                // Build the chart
+                $('#pie3').highcharts({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Resumo motivos'
+                    },
+                    credits:{
+                        enabled: false
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    series: [{
+                            name: "Brands",
+                            colorByPoint: true,
+                            data: [{
+                                    name: "Aprendizagem",
+                                    y: 3
+                                }, {
+                                    name: "Distância",
+                                    y: 12
+                                }, 
+                                {
+                                    name: "Doença",
+                                    y: 1
+                                },
+                                {
+                                    name: "Financeiro",
+                                    y: 52
+                                },
+                                {
+                                    name: "Frequência",
+                                    y: 2
+                                },
+                                {
+                                    name: "Gravides",
+                                    y: 3
+                                },
+                                {
+                                    name: "Mudança de cidade",
+                                    y: 5
+                                },
+                                {
+                                    name: "Não indentificação com o curso",
+                                    y: 16
+                                },
+                                {
+                                    name: "Notas baixas",
+                                    y: 4
+                                },
+                                {
+                                    name: "Outros",
+                                    y: 32
+                                },
+                                {
+                                    name: "Trabalho",
+                                    y: 23
+                                },{
+                                    name: "Transferencia para outra instituição",
+                                    y: 4
+                                }]
+                        }]
+                });
+            });
+        });
     };
 
 }
