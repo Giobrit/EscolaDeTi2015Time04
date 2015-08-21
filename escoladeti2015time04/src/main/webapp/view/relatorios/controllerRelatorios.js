2383f4b93699efbd631af2a15339d1198db16862
gabriel.carvalho <gabrielcapelcavalho@gmail.com>
2015-08-20 22:11:14 -0300

    #6524 Iniciado o relatorio de Relatorios Por Centro

--- escoladeti2015time04/src/main/webapp/view/relatorios/controllerRelatorios.js
+++ escoladeti2015time04/src/main/webapp/view/relatorios/controllerRelatorios.js
@@ -1,7 +1,7 @@
 AppModule.controller("controllerRelatorioResumido", controllerRelatorioResumido);
 AppModule.controller("controllerRelatorioPorCentro", controllerRelatorioPorCentro);
 
-function controllerRelatorioResumido($scope, $http, $routeParams, $location, growl) {
+function controllerRelatorioResumido($scope) {
 
     $scope.init = function () {
         $(function () {
@@ -76,7 +76,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                     title: {
                         text: 'Percentual Trancamento/Cancelamento/Transferência'
                     },
-                    credits:{
+                    credits: {
                         enabled: false
                     },
                     tooltip: {
@@ -124,7 +124,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                     title: {
                         text: 'Percentual Permanência'
                     },
-                    credits:{
+                    credits: {
                         enabled: false
                     },
                     tooltip: {
@@ -145,13 +145,13 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                             colorByPoint: true,
                             data: [{
                                     name: "CBS - Saúde",
-                                    y: 341
+                                    y: 41
                                 }, {
                                     name: "CETA - Exatas",
-                                    y: 361
+                                    y: 57
                                 }, {
                                     name: "CHSA - Humanas",
-                                    y: 305
+                                    y: 59
                                 }]
                         }]
                 });
@@ -172,7 +172,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                     title: {
                         text: 'Resumo motivos'
                     },
-                    credits:{
+                    credits: {
                         enabled: false
                     },
                     tooltip: {
@@ -197,7 +197,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                                 }, {
                                     name: "Distância",
                                     y: 14
-                                }, 
+                                },
                                 {
                                     name: "Doença",
                                     y: 25
@@ -233,7 +233,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                                 {
                                     name: "Trabalho",
                                     y: 64
-                                },{
+                                }, {
                                     name: "Transferencia para outra instituição",
                                     y: 213
                                 }]
@@ -256,7 +256,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                     title: {
                         text: 'Resumo motivos'
                     },
-                    credits:{
+                    credits: {
                         enabled: false
                     },
                     tooltip: {
@@ -281,7 +281,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                                 }, {
                                     name: "Distância",
                                     y: 12
-                                }, 
+                                },
                                 {
                                     name: "Doença",
                                     y: 1
@@ -317,7 +317,7 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
                                 {
                                     name: "Trabalho",
                                     y: 23
-                                },{
+                                }, {
                                     name: "Transferencia para outra instituição",
                                     y: 4
                                 }]
@@ -326,9 +326,154 @@ function controllerRelatorioResumido($scope, $http, $routeParams, $location, gro
             });
         });
     };
-
 }
-function controllerRelatorioPorCentro($scope, $http, $growl) {
 
+function controllerRelatorioPorCentro($scope) {
+    
+    $scope.init = function(){};
 
+    $scope.gerar = function(){
+        $(function () {
+
+            $(document).ready(function () {
+
+                // Build the chart
+                $('#pie5').highcharts({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Resumo motivos'
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: false
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    series: [{
+                            name: "Brands",
+                            colorByPoint: true,
+                            data: [{
+                                    name: "Aprendizagem",
+                                    y: 7
+                                }, {
+                                    name: "Distância",
+                                    y: 14
+                                },
+                                {
+                                    name: "Doença",
+                                    y: 25
+                                },
+                                {
+                                    name: "Financeiro",
+                                    y: 286
+                                },
+                                {
+                                    name: "Frequência",
+                                    y: 1
+                                },
+                                {
+                                    name: "Gravides",
+                                    y: 7
+                                },
+                                {
+                                    name: "Mudança de cidade",
+                                    y: 127
+                                },
+                                {
+                                    name: "Não indentificação com o curso",
+                                    y: 76
+                                },
+                                {
+                                    name: "Notas baixas",
+                                    y: 11
+                                },
+                                {
+                                    name: "Outros",
+                                    y: 177
+                                },
+                                {
+                                    name: "Trabalho",
+                                    y: 64
+                                }, {
+                                    name: "Transferencia para outra instituição",
+                                    y: 213
+                                }]
+                        }]
+                });
+            });
+        });
+        $(function () {
+            $('#pie6').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'column'
+                },
+                title: {
+                    text: ''
+                },
+                legend: {
+                    enabled: false
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: {
+                        rotation: 0,
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }},
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Atendimentos'
+                    }
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.y}</b>'
+                },
+                credits: {
+                    enabled: false
+                },
+                series: [{
+                        name: "Atendimentos",
+                        data: [
+                            ['CBS - Saúde', 382],
+                            ['CETA - Exatas', 418],
+                            ['CHSA - Humanas', 364]
+                        ]
+                    }],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            });
+        });
+    };
+    
 }
