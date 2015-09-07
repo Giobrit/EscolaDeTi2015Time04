542572d98e8123adbc8f10252de79d39de5a9b69
roney.campos <roney.1995@hotmail.com>
2015-09-07 02:34:31 -0300

    #6675 - Implementando busca dos dados do aluno do lyceum.

--- escoladeti2015time04/src/main/webapp/view/relatorioAcademico/controllerRelatorioAcademico.js
+++ escoladeti2015time04/src/main/webapp/view/relatorioAcademico/controllerRelatorioAcademico.js
@@ -2,4 +2,33 @@ AppModule.controller("controllerRelatorioAcademico", controllerRelatorioAcademic
 
 function controllerRelatorioAcademico($scope, $http, $routeParams, $location, growl, $timeout) {
 
+$scope.carregarAluno = function (ra) {
+        if (ra.length !== 8) {
+            setAtributosAluno({});
+            return;
+        }
+
+        $http.get("/lyceumClient/aluno/" + ra).success(onSuccess).error($scope.onError);
+
+        function onSuccess(data) {
+            setAtributosAluno(data);
+        }
+    };
+
+    $scope.setMatriculado = function (data) {
+        $scope.relatorioAcademico.matriculado = stringToBoolean(data);
+        
+    };
+    
+    function setAtributosAluno(aluno) {
+        $scope.relatorioAcademico.nomeAluno = aluno.nome;
+        $scope.relatorioAcademico.curso = aluno.curso;
+        $scope.relatorioAcademico.centro = aluno.centro;
+        $scope.relatorioAcademico.serieSemestre = aluno.serie;
+        $scope.relatorioAcademico.turno = aluno.turno;
+        $scope.relatorioAcademico.bolsaFinanciamento = aluno.bolsa;
+        $scope.relatorioAcademico.numeroReprovacoes = aluno.reprovacao;
+        $scope.relatorioAcademico.matriculado = (aluno.matriculado);
+        $scope.matriculadoSelecionado = aluno.matriculado;
+    }
 }
