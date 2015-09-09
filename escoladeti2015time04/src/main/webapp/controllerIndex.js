ac82a824cb2ed65fa472601326fef4747ba4e37b
roney.campos <roney.1995@hotmail.com>
2015-09-09 19:12:12 -0300

    #6675 - Adicionando icone ao menu de aluno.

--- escoladeti2015time04/src/main/webapp/controllerIndex.js
+++ escoladeti2015time04/src/main/webapp/controllerIndex.js
@@ -22,6 +22,7 @@ function controllerIndex($scope, $http, $cookies, $location, growl) {
         $scope.icones["Atendimento"] = "fa-comment-o";
         $scope.icones["Atendimento Preventivo"] = "fa-comment-o";
         $scope.icones["Atendimento Especial"] = "fa-comment-o";
+        $scope.icones["Aluno"] = "fa-user";
         $scope.icones["Sistema"] = "fa-cogs";
 
         var idUsuario = $cookies.get('login');
