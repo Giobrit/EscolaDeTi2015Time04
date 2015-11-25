6307e1257ecaa571ee3c8956f1b98bee05a5c48f
liz.okuzono <lizokuzono@gmail.com>
2015-11-25 18:11:17 -0200

    master alteracao

--- escoladeti2015time04/src/main/java/br/unicesumar/escoladeti2015time04/atendimento/especial/solicitacao/AtendimentoEspecialSolicitacaoRepository.java
+++
@@ -1,8 +0,0 @@
-
-package br.unicesumar.escoladeti2015time04.atendimento.especial.solicitacao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtendimentoEspecialSolicitacaoRepository extends JpaRepository<AtendimentoEspecialSolicitacao, Long> {
-    
-}
